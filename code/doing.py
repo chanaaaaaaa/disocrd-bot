4b0aff0d23c51b0c8a74b53fe8fa26570aa3f212
chanaaaaaaa <king960129@gmail.com>
2024-01-07 02:33:01 +0800

    nice

--- code/doing.py
+++ code/doing.py
@@ -155,6 +155,91 @@ def current_weather(address):
         print(Exception)
         return msg    # 如果取資料有發生錯誤，直接回傳 msg
 
+# 氣象預報函式
+def forecast(address):
+    area_list = {}
+    # 將主要縣市個別的 JSON 代碼列出
+    json_api = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
+            "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
+            "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
+            "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
+            "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
+            "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
+    msg = '找不到天氣預報資訊。'    # 預設回傳訊息
+    try:
+        code = 'CWA-53CDB49E-D455-4018-BE72-36E6B6700123'
+        url = f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization={code}&downloadType=WEB&format=JSON'
+        f_data = requests.get(url)   # 取得主要縣市預報資料
+        f_data_json = f_data.json()  # json 格式化訊息內容
+        location = f_data_json['cwaopendata']['dataset']['location']  # 取得縣市的預報內容
+        for i in location:
+            city = i['locationName']    # 縣市名稱
+            wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
+            mint8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最低溫
+            maxt8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最高溫
+            ci8 = i['weatherElement'][3]['time'][0]['parameter']['parameterName']    # 舒適度
+            pop8 = i['weatherElement'][4]['time'][0]['parameter']['parameterName']   # 降雨機率
+            area_list[city] = f'未來八小時{wx8}。最高溫 {maxt8} 度。最低溫 {mint8} 度。{ci8}。降雨機率 {pop8} %。'  # 組合成回傳的訊息，存在以縣市名稱為 key 的字典檔裡
+        for i in area_list:
+            if i in address:        # 如果使用者的地址包含縣市名稱
+                msg = area_list[i]  # 將 msg 換成對應的預報資訊
+                # 將進一步的預報網址換成對應的預報網址
+                url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/{json_api[i]}?Authorization={code}&elementName=WeatherDescription'
+                f_data = requests.get(url)  # 取得主要縣市裡各個區域鄉鎮的氣象預報
+                f_data_json = f_data.json() # json 格式化訊息內容
+                location = f_data_json['records']['locations'][0]['location']    # 取得預報內容
+                break
+        for i in location:
+            city = i['locationName']   # 取得縣市名稱
+            wd = i['weatherElement'][0]['time'][1]['elementValue'][0]['value']  # 綜合描述
+            if city in address:           # 如果使用者的地址包含鄉鎮區域名稱
+                msg = f'未來八小時天氣{wd}' # 將 msg 換成對應的預報資訊
+                break
+        return msg  # 回傳 msg
+    except:
+        return msg  # 如果取資料有發生錯誤，直接回傳 msg
+    
+# 空氣品質函式
+def aqi(address):
+    city_list, site_list ={}, {}
+    msg = '找不到空氣品質資訊。'
+    try:
+        # 2022/12 時氣象局有修改了 API 內容，將部份大小寫混合全改成小寫，因此程式碼也跟著修正
+        url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+        a_data = requests.get(url)             # 使用 get 方法透過空氣品質指標 API 取得內容
+        a_data_json = a_data.json()            # json 格式化訊息內容
+        for i in a_data_json['records']:       # 依序取出 records 內容的每個項目
+            city = i['county']                 # 取出縣市名稱
+            if city not in city_list:
+                city_list[city]=[]             # 以縣市名稱為 key，準備存入串列資料
+            site = i['sitename']               # 取出鄉鎮區域名稱
+            aqi = int(i['aqi'])                # 取得 AQI 數值
+            status = i['status']               # 取得空氣品質狀態
+            site_list[site] = {'aqi':aqi, 'status':status}  # 記錄鄉鎮區域空氣品質
+            city_list[city].append(aqi)        # 將各個縣市裡的鄉鎮區域空氣 aqi 數值，以串列方式放入縣市名稱的變數裡
+        for i in city_list:
+            if i in address: # 如果地址裡包含縣市名稱的 key，就直接使用對應的內容
+                # 參考 https://airtw.epa.gov.tw/cht/Information/Standard/AirQualityIndicator.aspx
+                aqi_val = round(statistics.mean(city_list[i]),0)  # 計算平均數值，如果找不到鄉鎮區域，就使用縣市的平均值
+                aqi_status = ''  # 手動判斷對應的空氣品質說明文字
+                if aqi_val<=50: aqi_status = '良好'
+                elif aqi_val>50 and aqi_val<=100: aqi_status = '普通'
+                elif aqi_val>100 and aqi_val<=150: aqi_status = '對敏感族群不健康'
+                elif aqi_val>150 and aqi_val<=200: aqi_status = '對所有族群不健康'
+                elif aqi_val>200 and aqi_val<=300: aqi_status = '非常不健康'
+                else: aqi_status = '危害'
+                msg = f'空氣品質{aqi_status} ( AQI {aqi_val} )。' # 定義回傳的訊息
+                break
+        for i in site_list:
+            if i in address:  # 如果地址裡包含鄉鎮區域名稱的 key，就直接使用對應的內容
+                msg = f'空氣品質{site_list[i]["status"]} ( AQI {site_list[i]["aqi"]} )'
+                break
+        return msg    # 回傳 msg
+    except:
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
+    
+
+
 @app.route("/", methods=['POST'])
 def linebot():
     body = request.get_data(as_text=True)    # 取得收到的訊息內容
@@ -171,9 +256,9 @@ def linebot():
             if json_data['events'][0]['message']['type'] == 'location':
                 address = json_data['events'][0]['message']['address'].replace('台','臺')
                 # 回覆爬取到的相關氣象資訊
-                reply_message(f'{address}\n\n{current_weather(address)}', reply_token, access_token)
+                reply_message(f'{address}\n\n{current_weather(address)}\n\n{aqi(address)}\n\n{forecast(address)}', reply_token, access_token)
                 print(address)
-            if json_data['events'][0]['message']['type'] == 'text':   # 如果 message 的類型是文字 text
+            elif json_data['events'][0]['message']['type'] == 'text':   # 如果 message 的類型是文字 text
                 text = json_data['events'][0]['message']['text']      # 取出文字
                 if text == '!help' or text == '！help':
                     line_bot_api.reply_message(reply_token,TextSendMessage(text='"雷達回波圖"\n傳送最新的台灣周圍雷達迴波圖\n"地震資訊"\n傳送最新有感地震資訊\n"指定地點當前氣象狀況"\n左下角+號 位置資訊\n以上')) 
@@ -185,7 +270,7 @@ def linebot():
                     push_message(msg[0], user_id, access_token)       # 傳送地震資訊 ( 用 push 方法，因為 reply 只能用一次 )
                     reply_image(msg[1], reply_token, access_token)    # 傳送地震圖片 ( 用 reply 方法 )
                 else:
-                    line_bot_api.reply_message(reply_token,TextSendMessage(text='別吵 有問題就用"!help"'))
+                    line_bot_api.reply_message(reply_token,TextSendMessage(text='好了別吵 有問題就用"!help"'))
     except:
         print('error')                       # 如果發生錯誤，印出 error
     return 'OK'                              # 驗證 Webhook 使用，不能省略
